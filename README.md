590ff2867acbfb9c3011184b5d930fd3bdd50459
Delta <XDelta@users.noreply.github.com>
2020-06-08 23:05:27 -0700

    Inital Commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # VRChat_Avatar_Collector
+Script used to collect/archive your currently worn public avatar from vrchat.
+
+## Install
+```bash
+$ pip install -r requirements.txt
+```
+## Usage
+Change directory to the location of the script
+Edit config.json with your vrchat username and password
+Run VRChat_Avatar_Collector.py
+
+Then every 60 seconds the script will poll vrchat api to check your current avatar,
+if you haven't gotten it before it will download the thumbnail and name it with the avatar id
+
+Shield: [![CC BY-SA 4.0][cc-by-sa-shield]][cc-by-sa]
+
+This work is licensed under a [Creative Commons Attribution-ShareAlike 4.0
+International License][cc-by-sa].
+
+[![CC BY-SA 4.0][cc-by-sa-image]][cc-by-sa]
+
+[cc-by-sa]: http://creativecommons.org/licenses/by-sa/4.0/
+[cc-by-sa-image]: https://licensebuttons.net/l/by-sa/4.0/88x31.png
+[cc-by-sa-shield]: https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg
---
+++ VRChat_Avatar_Collector.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Copyright 2020, Delta Wolf
+Released under the Creative Commons Attribution-ShareAlike 4.0 license
+
+6/8/20
+"""
+import json, os, shutil, time
+from datetime import datetime
+
+import vrcpy, requests
+
+def main():
+	client = vrcpy.Client()
+	log_file = open("VRChat_Avatar_Collector.log", mode='a+', encoding='utf-8', errors='ignore', buffering=1)
+
+	def end():
+		client.logout()
+		log_file.close()
+		exit()
+
+	def log(text):
+		log = datetime.now().strftime("%m/%d/%y - %H:%M:%S")
+		con = datetime.now().strftime("%H:%M:%S") #console doesn't need day
+		log_file.write("["+log+"] "+text+'\n')
+		print("["+con+"] "+text)
+
+	def create_dirs():
+		if not os.path.exists(DIR):
+			log("No directory named "+config["avatarFolder"]+", making one")
+			os.mkdir(DIR)
+		else:
+			log("Using existing avatar directory '"+config["avatarFolder"]+"'")
+
+	try:
+		config = json.load(open('config.json'))
+		captureCount = int(config["captureCount"]) #default 16 enough to collect all your current favorites in one go
+		captureFrequency = int(config["captureFrequency"]) #default 60 seconds to meet vrchat api guidelines
+	except Exception as e:
+		log("Error opening config.json")
+		log(str(e))
+		end()
+
+	DIR = os.path.join(os.getcwd(), config["avatarFolder"])
+
+	client.login(config["username"], config["password"])
+	b = client.fetch_me()
+	log("Logged in as "+b.displayName)
+
+	def avtr_grab():
+		b = client.fetch_me() #refresh user object
+		avtr_name = client.fetch_avatar(b.currentAvatar).name #should be a way to get the name of the avatar without a second fetch
+		avtr_id = b.currentAvatar
+		img = b.currentAvatarThumbnailImageUrl
+		log("------------------------")
+		log("Name: "+avtr_name)
+		log("ID: "+avtr_id)
+		log("Img: "+img) #sometimes gets api.vrchat.cloud img link instead of cloudfront thumbnail png
+		outputFile = os.path.join(DIR, avtr_id+".png")
+		log(outputFile)
+
+		if not os.path.exists(outputFile):
+			r = requests.get(img, allow_redirects=True, stream=True, headers={'User-Agent': ""}) #stopped by cf if useragent is missing, fine if empty, https://i.gifer.com/72nt.gif
+			r.raw.decode_content = True
+			with open(outputFile,'wb') as f:
+				shutil.copyfileobj(r.raw, f)
+			log("Got: "+avtr_name+":"+avtr_id)
+		else:
+			log("Avatar " + avtr_name+":"+avtr_id+" has already been downloaded")
+			log("Skipping")
+
+	create_dirs()
+	for x in range(0, captureCount):
+		avtr_grab()
+		time.sleep(captureFrequency)
+	end()
+
+if __name__ == "__main__":
+	main()
